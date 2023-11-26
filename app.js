7c6bb8b859d45eebd1997b2856978bfc4c6495a2
taruntammali <108152936+taruntammali@users.noreply.github.com>
2023-11-26 18:18:27 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ console.log('Is this working?');
 let viz;
 
 //Add Share Link to Tableau Public in here
-const url = "https://public.tableau.com/app/profile/tarun.tammali/viz/SchoolsinMontgomeryCountyVisualization/Dashboard2";
+const url = "https://public.tableau.com/views/SchoolsinMontgomeryCountyVisualization/Dashboard2?:language=en-US&:display_count=n&:origin=viz_share_link";
 
 const vizContainer = document.getElementById('vizContainer');
 const options = {
